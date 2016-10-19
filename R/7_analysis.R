867688032dfc2152395d67218e7b87c49387bbee
mcolvin <colvin.mike@gmail.com>
2016-10-18 21:44:01 -0500

    added post hoc tests
    added post hoc tests for lower basin mean weights for 2011 to 2016

--- R/7_analysis.R
+++ R/7_analysis.R
@@ -14,4 +14,40 @@ fit09<- lm(lwgh~llen*year_f+basin,lw)
 fit<- lm(lwgh~llen*year_f*basin,lw)
 
 
+# POST HOC TEST 
+
+x11v12<- rep(0,56)
+x11v12[10:11]<-c(1,-1)
+names(x11v12)<-names(coef(fit))
+x11v12<- matrix(x11v12,1)
+tt <- glht(fit, linfct = x11v12)
+summary(tt)
+
+contr<-expand.grid(basin=c("LB","UB"),
+	year_f=levels(lw$year_f))
+contr$contr<-0
+contr$contr[which(contr$year_f==2011 & 
+	contr$basin=="LB")]<-1
+contr$contr[which(contr$year_f==2011 & 
+	contr$basin=="LB")]<--1	
+
+
+yy<-cld(lsmeans(fit, ~ year_f |llen +basin, 
+	at = list(llen = log(800),
+	basin="LB",
+	year_f=factor(c(2011:2016),levels(lw$year_f))
+	)))
 	
+	
+pdat<-data.frame(basin=c("LB"),
+	year_f=levels(lw$year_f),llen=log(800))
+predict(fit,pdat)
+baci_pval<- contrast(yy, 
+	list(x11v12=contr$contr))
+yy<- confint(baci_pval)	
+	
+# EXTRACT P VALUE OF INTERACTION 	
+yy <- lsmeans(fit, ~SiteClass:Period)
+baci_pval<- contrast(yy, 
+	list(baci=c(-1,1,1,-1)))
+yy<- confint(baci_pval)
